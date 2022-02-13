9f12f9daa3fa0039ff25483f02690f0ef0f05336
fuadhasan3 <hasanfuad170@gmail.com>
2022-02-14 01:52:17 +0600

    is NaN

--- js/app.js
+++ js/app.js
@@ -17,8 +17,16 @@ function generatePin(){
 document.getElementById('key-pad').addEventListener('click', function(event){
     const number = event.target.innerText;
     const calcInput = document.getElementById('typed-numbers');
-    const previousNumber = calcInput.value;
-    const newNumber = previousNumber + number;
-    calcInput.value = newNumber;
+    if (isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+        
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
 
 });
